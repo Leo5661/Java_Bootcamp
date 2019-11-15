26ac9cc71638ee3921cb48fb10fc1fdf2dc8bc1c
Leo5661 <singhra66@gmail.com>
2019-11-15 08:36:19 +0530

    java code for bootcamp

---
+++ Area.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Area {
+   public static void main (String[] args)
+   {
+       System.out.println("Enter Side of Square:");
+       //Capture the user's input
+        
+       //Storing the captured value in a variable
+       
+       //Area of Square = side*side
+       
+      
+   }
+}
---
+++ main.java
@@ -0,0 +1,11 @@
+// Data Type
+
+// Variable
+
+// Methods
+
+// Comments in Java
+
+// Class
+
+// Take input
