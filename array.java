60c5ddf7e3ad9e49616bf18637526f8e945995db
Rahul Singh <singhra66@gmail.com>
2019-11-15 08:41:28 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,23 @@
+// array
+
+
+
+
+
+
+
+
+
+/*
+class SumOfArray{
+   public static void main(String args[]){
+      int[] array = {};
+      
+      //Advanced for loop
+      for( int num : array) {
+          sum = sum+num;
+      }
+      
+   }
+}
+*/
